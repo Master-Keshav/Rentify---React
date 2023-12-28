108e45840659a8c26a7296451d380adb41966ee1
Keshav Vyas <keshavvyas484@gmail.com>
2023-12-28 22:52:22 +0530

    vercel config changes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+// rentify/client/vite.config.ts
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default {
+  base: '/rentify/client/',
   plugins: [react()],
-})
+}
