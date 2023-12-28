5459dc3c81185fd11846aeb14f4233894e7e2299
Keshav Vyas <keshavvyas484@gmail.com>
2023-12-28 23:31:17 +0530

    configuring db.js

--- server/db.js
+++ server/db.js
@@ -1,18 +1,18 @@
 const mongoose = require("mongoose");
 
-module.exports = () => {
-	const connectionParams = {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	};
-	mongoose.connect(process.env.DB, connectionParams, (err) => {
-		if (err) {
-			console.log(err)
-			console.log("Could not connect database!");
-		}
-		else {
-			console.log("Logged in successfully!")
-			console.log("Connected to database successfully");
-		}
-	})
+module.exports = async () => {
+	try {
+		const connectionParams = {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		};
+		
+		console.log("env - ", process.env.DB)
+		await mongoose.connect(process.env.DB, connectionParams);
+
+		console.log("Connected to MongoDB successfully");
+	} catch (error) {
+		console.error("Error connecting to MongoDB:", error.message);
+		throw error; // Rethrow the error to stop the application if the connection fails
+	}
 };
